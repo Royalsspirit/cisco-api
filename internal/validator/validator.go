a11f614594a3154db8eff5a39b6938c479906885
gmajkic <gmajkic@vente-privee.com>
2020-01-11 17:20:27 +0100

    feat(validator): initilize validator to check input data

---
+++ internal/validator/validator.go
@@ -0,0 +1,30 @@
+package validator
+
+import (
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
+)
+
+// Val represents the Valdiator
+type Val struct {
+	Val   *validator.Validate
+	Trans ut.Translator
+}
+
+// NewValidator create a new Validator
+func NewValidator() *Val {
+	eng := en.New()
+	uni := ut.New(eng, eng)
+
+	trans, _ := uni.GetTranslator("en")
+
+	err := en_translations.RegisterDefaultTranslations(v, trans)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return &Val{Val: v, Trans: trans}
+}
